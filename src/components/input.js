b5efeae491bbc6a715eb16a1d0d86c4eeb0137b4
BlinovaTA <tanyushablinova@gmail.com>
2021-03-31 21:02:00 +0500

    Переделала Input в функциональный компонент

--- src/components/input.js
+++ src/components/input.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-export default class Input extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired
-  }
 
-  render() {
-    const { title, name, type, value, onChange } = this.props;
-
-    return (
-      <div className='form__block'>
-        <label htmlFor={name} className='form__item-title'>{title}</label>
-        <input type={type} className='form__input' name={name} id={name} value={value} onChange={onChange} />
-      </div>
-    )
-  }
+const Input = ({ title, name, type, value, onChange }) => {
+  return (
+    <div className='form__block'>
+      <label htmlFor={name} className='form__item-title'>{title}</label>
+      <input type={type} className='form__input' name={name} id={name} value={value} onChange={onChange} />
+    </div>
+  )
 }
+
+Input.propTypes = {
+  title: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+}
+
+export default Input;
